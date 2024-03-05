66d7bfed9cbe6ffd1bf61f38ce6bfa3c316e1b8c
Valeriia <valeriiashbn@gmail.com>
2024-03-04 17:06:00 -0800

    test failure fix

--- src/test/java/org/example/BonusServiceTest.java
+++ src/test/java/org/example/BonusServiceTest.java
@@ -12,7 +12,7 @@ public class BonusServiceTest {
         // подготавливаем данные:
         long amount = 1000;
         boolean registered = false;
-        long expected = 30;
+        long expected = 10;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -36,8 +36,8 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
-    
-        @Test
+
+    @Test
     void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
